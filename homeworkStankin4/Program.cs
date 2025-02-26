3860157272c0dc8cd62b0c619ed3c238e5a1a2ea
samai <samai@DESKTOP-D951K14>
2025-02-27 02:15:53 +0300

    Добавьте файлы проекта.

---
+++ homeworkStankin4/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace homeworkStankin4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите минимальное число списка: ");
+            Int32.TryParse(Console.ReadLine(), out int minNumber);
+            Console.Write("Введите максимальное число списка: ");
+            Int32.TryParse(Console.ReadLine(), out int maxNumber);
+            Console.Write("Введите, чему равен шаг в списке: ");
+            Int32.TryParse(Console.ReadLine(), out int step);
+            if(maxNumber < minNumber)
+            {
+                Console.WriteLine("Максимальное число не может быть меньше минимального!");
+            }
+            else if(maxNumber - minNumber <= step)
+            {
+                Console.WriteLine("Разница между максимальным и минимальным числом должна быть больше шага!");
+            }
+            else
+            {
+                Print(createList(minNumber, maxNumber, step));
+            }
+                
+        }
+        static List<int> createList(int minNumber, int maxNumber, int step)
+        {
+            List <int> numbers = [];
+            for(int i = minNumber; i <= maxNumber; i += step)
+            {
+                numbers.Add(i);
+            }
+            return numbers;
+        }
+
+        static void Print(List<int> numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ homeworkStankin4/withMassive.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeworkStankin4
+{
+    class withMassive
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите минимальное число списка: ");
+            Int32.TryParse(Console.ReadLine(), out int minNumber);
+            Console.Write("Введите максимальное число списка: ");
+            Int32.TryParse(Console.ReadLine(), out int maxNumber);
+            Console.Write("Введите, чему равен шаг в списке: ");
+            Int32.TryParse(Console.ReadLine(), out int step);
+            if (maxNumber < minNumber)
+            {
+                Console.WriteLine("Максимальное число не может быть меньше минимального!");
+            }
+            else if (maxNumber - minNumber <= step)
+            {
+                Console.WriteLine("Разница между максимальным и минимальным числом должна быть больше шага!");
+            }
+            else
+            {
+                Print(createList(minNumber, maxNumber, step));
+            }
+
+        }
+        static int[] createList(int minNumber, int maxNumber, int step)
+        {
+            int[] numbers = new int[];
+            for (int i = minNumber; i <= maxNumber; i += step)
+            {
+                numbers[i] = i;
+            }
+            return numbers;
+        }
+
+        static void Print(int[] numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
